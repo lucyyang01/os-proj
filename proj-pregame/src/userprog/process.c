e9c1486c2e8c3848c61d1a26bbc7629a8ff10098
Lucy <lyang01@berkeley.edu>
2023-09-12 01:03:37 +0000

    yayy

--- proj-pregame/src/userprog/process.c
+++ proj-pregame/src/userprog/process.c
@@ -473,7 +473,8 @@ static bool setup_stack(void** esp) {
   if (kpage != NULL) {
     success = install_page(((uint8_t*)PHYS_BASE) - PGSIZE, kpage, true);
     if (success)
-      *esp = PHYS_BASE;
+      *esp = PHYS_BASE - 20;
+      //*esp = PHYS_BASE;
     else
       palloc_free_page(kpage);
   }
