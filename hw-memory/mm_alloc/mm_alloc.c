2d6360b17191e4c864f9ac16cb948fab18523618
Lucy <lyang01@berkeley.edu>
2023-11-06 08:47:41 +0000

    basic implementation of malloc

--- hw-memory/mm_alloc/mm_alloc.c
+++ hw-memory/mm_alloc/mm_alloc.c
@@ -8,11 +8,63 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
+
+static struct memory_block_node* mem_head = NULL;
+
+static struct memory_block_node* mem_tail = NULL;
+
 
 void* mm_malloc(size_t size) {
   //TODO: Implement malloc
 
-  return NULL;
+  if (size == 0)
+    return NULL;
+  //initialize list if it's empty
+  if (mem_head == NULL) {
+    mem_head = (struct memory_block_node*) sbrk(sizeof(struct memory_block_node));
+    mem_head->prev = NULL;
+    mem_head->free = false;
+    mem_head->next = NULL;
+    mem_head->size = size;
+    __uint8_t start = sbrk(size);
+    mem_head->allocated = start;
+    mem_tail = mem_head;
+    return mem_head->allocated;
+  }
+
+  //iterate through mem to see if there's a block big enough
+  struct memory_block_node* curr = mem_head;
+  while(curr != NULL) {
+    //if we find a sufficiently large block
+    if(curr->size >= size) {
+      //if the current block can hold anohter block
+      if (curr->size > size + sizeof(struct memory_block_node)) {
+        //split the current block
+        struct memory_block_node* new_block = (struct memory_block_node*) sbrk(sizeof(struct memory_block_node));
+        new_block->prev = mem_tail;
+        new_block->next = NULL;
+        new_block->size = curr->size - size - sizeof(struct memory_block_node);
+        new_block->free = true;
+        new_block->allocated = (__uint8_t) sbrk(new_block->size);
+        memset(new_block->allocated, 0, curr->size - size - sizeof(struct memory_block_node));
+        return curr->allocated;
+      }
+      //current block can't accommodate another block
+      return curr->allocated;
+    }
+    curr = curr->next;
+  }
+
+  //if we haven't returned by this point we need to allocate a new block
+  struct memory_block_node* new_block1 = (struct memory_block_node*) sbrk(sizeof(struct memory_block_node));
+  new_block1->prev = mem_tail;
+  new_block1->next = NULL;
+  new_block1->size = size;
+  new_block1->free = false;
+  new_block1->allocated = (__uint8_t) sbrk(new_block1->size);
+  memset(new_block1->allocated, 0, size);
+  return new_block1->allocated;
 }
 
 void* mm_realloc(void* ptr, size_t size) {
--- hw-memory/mm_alloc/mm_alloc.h
+++ hw-memory/mm_alloc/mm_alloc.h
@@ -10,6 +10,16 @@
 #define _malloc_H_
 
 #include <stdlib.h>
+#include <stdbool.h>
+
+/* struct to store blocks of memory */
+struct memory_block_node {
+    struct memory_block_node* prev; /* prev, next are pointers to adjacent blocks of memory*/
+    struct memory_block_node* next;
+    bool free; /* if this block of memory is free*/
+    __uint32_t size; /* size of allocated memory block */
+    __uint8_t allocated; 
+};
 
 void* mm_malloc(size_t size);
 void* mm_realloc(void* ptr, size_t size);
